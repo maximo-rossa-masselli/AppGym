0e3ef8e60fb15239ad06380624e6d4b8d60f36a6
unknown <maximorossam@gmail.com>
2026-02-27 22:55:24 -0300

    cambios en setting.py para que pueda entrar a admin

--- backend/routines/utils.py
+++ backend/routines/utils.py
@@ -1,12 +1,18 @@
 import qrcode
 from io import BytesIO
 from django.core.files import File
+import os
 
 
 def generar_qr_para_gym(gym):
-    url = f"http://127.0.0.1:8000/api/public/{gym.qr_token}/"
-
-    qr = qrcode.make(url)
+    base_url = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+    if base_url:
+        print("Usando url de render")
+        full_url = f"https://{base_url}/public/{gym.qr_token}/"
+    else:
+        print("Usando url localhost")
+        full_url = f"http://127.0.0.1:8000/api/public/{gym.qr_token}/"
+    qr = qrcode.make(full_url)
 
     buffer = BytesIO()
     qr.save(buffer, format="PNG")
