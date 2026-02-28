5a8a17591e6c97aa74a7abe1c634da9430cf5f93
unknown <maximorossam@gmail.com>
2026-02-27 22:17:37 -0300

    Cambios en settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='alberto1966')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
