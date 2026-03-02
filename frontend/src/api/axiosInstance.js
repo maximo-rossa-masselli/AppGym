0370adfcb8c33a4fa1c61a97484e32e9525d2a48
unknown <maximorossam@gmail.com>
2026-03-02 15:00:21 -0300

    Se agregó la URL de producción al axios

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// Vite usa 'import.meta.env' para acceder a las variables
+// Si la variable no existe, cae al localhost por defecto
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: `${API_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
