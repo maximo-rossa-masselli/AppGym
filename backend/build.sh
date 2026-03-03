715332d31fda582f5f2e782d71db00da379b267f
unknown <maximorossam@gmail.com>
2026-03-03 12:52:53 -0300

    Se agrego comando para crear superuser

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Instalar dependencias
 pip install -r requirements.txt
 
+# 2. Recolectar estáticos
 python manage.py collectstatic --no-input
 
+# 3. Correr migraciones (esto ya lo tenías)
 python manage.py migrate
+
+# 4. Crear el superusuario de forma automática (LA NUEVA PARTE)
+python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User = get_user_model(); User.objects.filter(username=os.getenv('ADMIN_USER')).exists() or User.objects.create_superuser(os.getenv('ADMIN_USER'), os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'))"
